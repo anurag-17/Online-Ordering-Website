b2dbafeca19453230fa31de1e4909c30ed9ce871
anurag-17 <anurag.pandey17@gmail.com>
2024-02-20 19:24:44 +0530

    initial setup

---
+++ client/src/app/page.js
@@ -0,0 +1,10 @@
+import Login from "@/components/admin/auth-component/Login";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div> 
+    <Login/>
+  </div>
+  );
+}
---
+++ client/src/components/admin/auth-component/RightSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const RightSection = () => {
+    return (
+        <div className="block lg:w-[50%] px-[10px] lg:px-0">
+            <img
+                // src={bgImg}
+                alt="img"
+                className="w-full h-auto mx-auto"
+            />
+        </div>)
+};
+
+export default RightSection;
